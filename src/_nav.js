64155f3bdfc1581254136db2f496049ed3c019f9
Hichem Bouzara <fa_bouzara@esi.dz>
2019-02-19 21:06:07 +0100

    First commit

---
+++ src/_nav.js
@@ -0,0 +1,76 @@
+export default {
+  items: [
+    {
+      name: 'Dashboard',
+      url: '/dashboard',
+      icon: 'icon-speedometer',
+      badge: {
+        variant: 'info',
+        text: 'NEW',
+      },
+    },
+    {
+      title: true,
+      name: 'Admin',
+    },
+    {
+      name: 'Pages',
+      url: '/pages',
+      icon: 'icon-pencil',
+      children: [
+        {
+          name: 'Login',
+          url: '/login',
+          icon: 'icon-star',
+        },
+        {
+          name: 'Register',
+          url: '/register',
+          icon: 'icon-star',
+        },
+        {
+          name: 'Error 404',
+          url: '/404',
+          icon: 'icon-star',
+        },
+        {
+          name: 'Error 500',
+          url: '/500',
+          icon: 'icon-star',
+        },
+      ],
+    },
+    {
+      title: true,
+      name: 'Fabricant',
+    },
+    {
+      name: 'Pages',
+      url: '/pages',
+      icon: 'icon-pencil',
+      children: [
+        {
+          name: 'Login',
+          url: '/login',
+          icon: 'icon-star',
+        },
+        {
+          name: 'Register',
+          url: '/register',
+          icon: 'icon-star',
+        },
+        {
+          name: 'Error 404',
+          url: '/404',
+          icon: 'icon-star',
+        },
+        {
+          name: 'Error 500',
+          url: '/500',
+          icon: 'icon-star',
+        },
+      ],
+    },
+
+  ],
+};
