81857be65d4132938d8b716f4e40eb105b9e3e63
Bouchebaba Amine <ABouchebaba@outlook.com>
2019-03-27 18:31:53 +0100

    added brands managing, tests not done yet, image uploading via URL

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
-import { combineReducers } from 'redux';
-import authReducer from './authReducer';
-import errorReducer from './errorReducer';
-import fabricantReducer from './fabricantReducer';
+import { combineReducers } from "redux";
+import authReducer from "./authReducer";
+import errorReducer from "./errorReducer";
+import fabricantReducer from "./fabricantReducer";
+import brandReducer from "./brandReducer";
 
 export default combineReducers({
   auth: authReducer,
   errors: errorReducer,
-  fabricant: fabricantReducer
+  fabricant: fabricantReducer,
+  brand: brandReducer
 });
